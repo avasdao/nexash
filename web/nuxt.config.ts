d773154385448f0b3290291dec7271d08c6ac991
Ava's DAO Contributors <support@avasdao.org>
2023-01-23 04:33:50 -0500

    Add Matomo analytics.

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            // charset: 'utf-16',
+            // viewport: 'width=500, initial-scale=1',
+            title: 'Nexa Shell — Default Title',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { hid: 'description', name: 'description', content: 'Data queries.' },
+                { name: 'format-detection', content: 'telephone=no' }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ],
+            script: [
+                { src: '/js/matomo.js' },
+            ],
+        },
+    },
+
     modules: ['@nuxtjs/tailwindcss']
 })
