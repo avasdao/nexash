be12a734d20bbf4a9af2bca0fc74d31be4d81575
Ava's DAO Contributors <support@avasdao.org>
2023-01-27 01:36:26 -0500

    Update hero.

---
+++ web/components/HeroView.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- Hero section -->
+    <main class="relative">
+        <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"></div>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+                <div class="absolute inset-0">
+                    <img
+                        class="h-full w-full object-cover"
+                        src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                        alt="People working on laptops"
+                    />
+                    <div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div>
+                </div>
+
+                <div class="relative py-16 px-6 sm:py-24 lg:py-32 lg:px-8">
+                    <h1 class="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                        <span class="block text-white">
+                            Best-in-class Cloud Data
+                        </span>
+
+                        <span class="block text-indigo-200">
+                            for Nexa Builders
+                        </span>
+                    </h1>
+                    <p class="mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl">
+                        If you're a Builder in the Nexa community then you've come to the right place.
+                        Choose from our library of SDKs with direct access ANY data on the blockchain.
+                    </p>
+                    <div class="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                        <div class="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                            <a href="javascript://" class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-2xl font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8">
+                                Get Started
+                            </a>
+
+                            <a href="javascript://" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-2xl font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8">
+                                Watch a Demo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -13,39 +13,7 @@ useHead({
         <HeaderView />
 
         <main>
-            <!-- Hero section -->
-            <div class="relative">
-                <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"></div>
-                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-                        <div class="absolute inset-0">
-                            <img
-                                class="h-full w-full object-cover"
-                                src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                                alt="People working on laptops"
-                            />
-                            <div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div>
-                        </div>
-                        <div class="relative py-16 px-6 sm:py-24 lg:py-32 lg:px-8">
-                            <h1 class="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
-                                <span class="block text-white">Take control of your</span>
-                                <span class="block text-indigo-200">customer support</span>
-                            </h1>
-                            <p class="mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                            </p>
-                            <div class="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                                <div class="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                                    <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8">Get started</a>
-                                    <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8">
-                                        Live demo
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <HeroView />
 
             <!-- Logo Cloud -->
             <div class="bg-gray-100">
