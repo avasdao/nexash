a0f14c731053d7f3ce76cca97f309d73d73a5b01
Ava's DAO Contributors <support@avasdao.org>
2023-01-28 07:18:34 -0500

    Remove unused endpoints.

--- api/server.js
+++ api/server.js
@@ -28,21 +28,6 @@ app.post('/v1/admin', require('./routes/admin'))
 /* Initialize Core (Node) route. */
 app.post('/v1/core', require('./routes/core'))
 
-/* Initialize Magic (Email) Link route. */
-app.post('/v1/magiclink', require('./routes/magiclink'))
-
-/* Initialize Notifications route. */
-app.post('/v1/notifs', require('./routes/notifs'))
-
-/* Initialize Orders route. */
-app.post('/v1/orders', require('./routes/orders'))
-
-/* Initialize Sessions route. */
-app.post('/v1/sessions', require('./routes/sessions'))
-
-/* Initialize Sideshift route. */
-app.post('/v1/sideshift', require('./routes/sideshift'))
-
 // TODO: Offer help.
 app.get('/v1', (req, res) => {
     res.end('Oops! I think you forgot something.')
@@ -59,7 +44,6 @@ console.info('Current Environment Variables')
 console.info('-----------------------------')
 console.info('  - NODE_ENV       :', process.env.NODE_ENV)
 console.info('  - COUCHDB_AUTH   :', process.env.COUCHDB_AUTH)
-console.info('  - MAGIC_LINK_KEY :', process.env.MAGIC_LINK_KEY)
 console.info('  - SMTP_HOST      :', process.env.SMTP_HOST)
 console.info('  - SMTP_USER      :', process.env.SMTP_USER)
 console.info('  - SMTP_PASS      :', process.env.SMTP_PASS)
