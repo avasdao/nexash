0e6e3911dd964bd3a4dc37c7668b6d49b937f180
Ava's DAO Contributors <support@avasdao.org>
2023-01-28 03:23:07 -0500

    Add universal handler.

---
+++ web/pages/[...slug].vue
@@ -0,0 +1,33 @@
+<script setup>
+useHead({
+    title: 'Universal Handler - Nexa Shell',
+    meta: [{
+        name: 'description',
+        content: 'Universal Handler'
+    }]
+})
+
+const route = useRoute()
+
+console.log('ROUTE PARAMS', route.params)
+
+const slug = route.params.slug
+</script>
+
+<template>
+    <main class="">
+        <HeaderView />
+
+        <main class="max-w-7xl mx-auto">
+            <h1 class="text-4xl font-medium">
+                Universal Handler
+            </h1>
+
+            <span class="block text-lg font-medium">
+                SLUG -> {{ slug }}
+            </span>
+        </main>
+
+        <FooterView />
+    </main>
+</template>
