c84b07912fd7a5921ba759e969dbacb4f149b720
Ava's DAO Contributors <support@avasdao.org>
2023-01-28 03:15:20 -0500

    Add first dynamic page.

---
+++ web/pages/Blocks.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+    title: 'Blocks - Nexa Shell',
+    meta: [{
+        name: 'description',
+        content: 'Blocks'
+    }]
+})
+</script>
+
+<template>
+    <main class="">
+        <HeaderView />
+
+        <main>
+            Blocks
+        </main>
+
+        <FooterView />
+    </main>
+</template>
---
+++ web/pages/tx/[txid].vue
@@ -0,0 +1,33 @@
+<script setup>
+useHead({
+    title: 'Transaction - Nexa Shell',
+    meta: [{
+        name: 'description',
+        content: 'Transaction'
+    }]
+})
+
+const route = useRoute()
+
+console.log('ROUTE PARAMS', route.params)
+
+const txid = route.params.txid
+</script>
+
+<template>
+    <main class="">
+        <HeaderView />
+
+        <main class="max-w-7xl mx-auto">
+            <h1 class="text-4xl font-medium">
+                Transaction
+            </h1>
+
+            <span class="block text-lg font-medium">
+                {{ txid }}
+            </span>
+        </main>
+
+        <FooterView />
+    </main>
+</template>
