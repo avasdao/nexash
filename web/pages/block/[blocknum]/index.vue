2299e1ba7e55a1be49ecbd01c8f63cdfde0f8053
Ava's DAO Contributors <support@avasdao.org>
2023-01-28 04:46:30 -0500

    Add WebSockets support.

---
+++ web/pages/block/[blocknum]/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+useHead({
+    title: 'Block - Nexa Shell',
+    meta: [{
+        name: 'description',
+        content: 'Block'
+    }]
+})
+
+const route = useRoute()
+
+console.log('ROUTE PARAMS', route.params)
+
+const blocknum = route.params.blocknum
+</script>
+
+<template>
+    <main class="">
+        <HeaderView />
+
+        <main class="max-w-7xl mx-auto">
+            <h1 class="text-4xl font-medium">
+                Block
+            </h1>
+
+            <p>
+                {{blocknum}}
+            </p>
+        </main>
+
+        <FooterView />
+    </main>
+</template>
