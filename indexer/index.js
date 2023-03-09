802b0d5e6b1dcdcfbd9462043b2be87530fdfec1
Ava's DAO Contributors <support@avasdao.org>
2023-03-09 04:12:12 -0500

    Enable Indexer daemon.

--- indexer/index.js
+++ indexer/index.js
@@ -1,5 +1,6 @@
 /* Import modules. */
 import { call } from '@nexajs/rpc'
+import zmq from 'zeromq'
 
 const getBlockchainInfo = async () => {
     /* Set method. */
@@ -21,8 +22,20 @@ const getBlockchainInfo = async () => {
     console.log('\nJSON-RPC response:\n%s', response)
 }
 
-console.log('\n  Starting Nexa Shell Daemon...')
+console.info('\n  Starting Nexa Shell (Indexer) daemon...\n')
 
 ;(async () => {
     await getBlockchainInfo()
+
+    const sock = new zmq.Subscriber
+
+    sock.connect('tcp://127.0.0.1:28332')
+
+    sock.subscribe()
+    console.log(`Subscriber connected to port 28332`)
+
+    for await (const [ topic, msg ] of sock) {
+        console.log('received a message related to:', Buffer.from(topic).toString(), 'containing message:', Buffer.from(msg).toString('hex'), '\n')
+    }
+
 })()
