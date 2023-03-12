890e39b8ca7da4e60a0f13902de63dec92e73e50
Ava's DAO Contributors <support@avasdao.org>
2023-03-12 11:13:38 -0400

    Add CORS support.

--- graphql/server.js
+++ graphql/server.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { buildSchema } from 'graphql'
@@ -13,19 +14,28 @@ const schema = buildSchema(`
 `)
 
 // The root provides a resolver function for each API endpoint
-const root = {
+const rootValue = {
     hello: () => {
         return 'Hello world!'
     },
 }
 
+const graphiql = true
+
+const graphqlOptions = {
+    schema,
+    rootValue,
+    graphiql,
+}
+
+/* Initialize application. */
 const app = express()
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}))
+/* Enable CORS. */
+app.use(cors())
+
+/* Setup GraphQL endpoint. */
+app.use('/graphql', graphqlHTTP(graphqlOptions))
 
 app.listen(PORT)
 console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`)
