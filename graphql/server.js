16a6d43a602e9b12443e6548acdbbc8fb376dbd1
Ava's DAO Contributors <support@avasdao.org>
2023-03-13 08:25:04 -0400

    Adjust rate limit.

--- graphql/server.js
+++ graphql/server.js
@@ -162,7 +162,7 @@ app.use(cors())
 
 /* Set rate limits. */
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 2 * 60 * 1000, // 2 minutes
 	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
