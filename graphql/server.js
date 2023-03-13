9583758e1e5eb496f6a04b52b4c1f2d1881dfe4c
Ava's DAO Contributors <support@avasdao.org>
2023-03-13 06:18:11 -0400

    Update schema.

--- graphql/server.js
+++ graphql/server.js
@@ -9,8 +9,6 @@ const PORT = 3000
 // NOTE: Construct a schema, using GraphQL schema language.
 const schema = buildSchema(`
   type Query {
-    hello(chain: String): String
-
     addr(base58: String, script: String): Address
     addrs(base58: [String], script: [String]): [Address]
 
@@ -47,73 +45,92 @@ const schema = buildSchema(`
   }
 `)
 
-const SAMPLE_TOKEN = {
-    txid: 'my-leet-tokenid',
-    amount: 888.00
-}
-
-const SAMPLE_TX = {
-    txid: 'my-leet-txid',
-    txidem: 'my-leet-txidem',
-    amount: 1337.00
-}
-
 // NOTE: The root provides a resolver function for each API endpoint.
 const rootValue = {
-    hello: (_args) => {
-        /* Set chain. */
-        const chain = _args?.chain || 'Crypto'
-
-        /* Return greeting. */
-        return `Hello ${chain} world!`
-    },
-
     addr: (_args) => {
+        /* Set base58. */
+        const base58 = _args?.base58 || 'nexa:my-awesome-address'
+
         return {
-            base58: 'nexa:my-awesome-address',
+            base58,
             script: '001840888777666555444333222111',
             type: 'template',
         }
     },
 
     addrs: (_args) => {
+        /* Set base58. */
+        const base58 = _args?.base58 || 'nexa:my-awesome-address'
+
         return [{
-            base58: 'nexa:my-awesome-address',
+            base58,
             script: '001840888777666555444333222111',
             type: 'template',
         }]
     },
 
     block: (_args) => {
+        /* Set height. */
+        const height = _args?.height || 201337
+
         return {
-            height: 1337,
+            height,
             hash: 'my-leet-hash',
             txs: [SAMPLE_TX],
         }
     },
 
     blocks: (_args) => {
+        /* Set height. */
+        const height = _args?.height || 201337
+
         return [{
-            height: 1337,
+            height,
             hash: 'my-leet-hash',
             txs: [SAMPLE_TX],
         }]
     },
 
     token: (_args) => {
-        return SAMPLE_TOKEN
+        /* Set tokenid. */
+        const tokenid = _args?.tokenid || 'my-leet-tokenid'
+
+        return {
+            tokenid,
+            amount: 888.00
+        }
     },
 
     tokens: (_args) => {
-        return [SAMPLE_TOKEN]
+        /* Set tokenid. */
+        const tokenid = _args?.tokenid || 'my-leet-tokenid'
+
+        return [{
+            tokenid,
+            amount: 888.00
+        }]
     },
 
     tx: (_args) => {
-        return SAMPLE_TX
+        /* Set txidem. */
+        const txidem = _args?.txidem || 'my-leet-txidem'
+
+        return {
+            txid: 'my-leet-txid',
+            txidem,
+            amount: 1337.00
+        }
     },
 
     txs: (_args) => {
-        return [SAMPLE_TX]
+        /* Set txidem. */
+        const txidem = _args?.txidem || 'my-leet-txidem'
+
+        return [{
+            txid: 'my-leet-txid',
+            txidem,
+            amount: 1337.00
+        }]
     },
 }
 
