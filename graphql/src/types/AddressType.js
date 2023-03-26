93e04ec2dd87367c5a8ae9b15cc27b6558f38819
Ava's DAO Contributors <support@avasdao.org>
2023-03-26 19:52:28 -0400

    Update format.

--- graphql/src/types/AddressType.js
+++ graphql/src/types/AddressType.js
@@ -6,22 +6,22 @@ export default new GraphQLEnumType({
     values: {
         P2PKH: {
             value: 'P2PKH',
-            description: `Type ID is __0.__`,
+            description: `Type ID is __0__`,
         },
 
         SCRIPT: {
             value: 'SCRIPT',
-            description: `Type ID is __1<<3__ _(or 8)._`,
+            description: `Type ID is __1<<3__ _(or 8)_`,
         },
 
         TEMPLATE: {
             value: 'TEMPLATE',
-            description: `Type ID is __19<<3__ _(or 0xC8|200)._`,
+            description: `Type ID is __19<<3__ _(or 0xC8|200)_`,
         },
 
         GROUP: {
             value: 'GROUP',
-            description: `Type ID is __11<<3__ _(or 0x88|136)._`,
+            description: `Type ID is __11<<3__ _(or 0x88|136)_`,
         },
     },
     description: `Select from one of the compatible __Address Types__ supported by the Nexa protocol.`,
