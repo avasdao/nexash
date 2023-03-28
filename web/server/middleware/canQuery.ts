d29aef48a7b5a475d9cdabe6c5e0be8b98e59095
Ava's DAO Contributors <support@avasdao.org>
2023-03-28 18:57:43 -0400

    Add new middleware.

---
+++ web/server/middleware/canQuery.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler((event) => {
+    /* Set (request) query. */
+    const query = getQuery(event)
+    console.log('canQuery (query):', query)
+
+    if (query?.query) {
+        // NOTE: Accepted
+        setResponseStatus(event, 202)
+
+        return 'ok'
+        // return 'no'
+        // return 'data required'
+    }
+})
---
+++ web/server/middleware/useJson.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler((event) => {
+    /* Set (request) query. */
+    const query = getQuery(event)
+    console.log('canQuery (query):', query)
+
+    if (query?.json) {
+        return {
+            hi: 'JSON query!',
+        }
+    }
+})
