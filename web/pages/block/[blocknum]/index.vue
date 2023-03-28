4e0872e37fc1770b6c1c85851fcaef872ea4871e
Ava's DAO Contributors <support@avasdao.org>
2023-03-28 04:12:56 -0400

    Add BlockItem.

--- web/pages/block/[blocknum]/index.vue
+++ web/pages/block/[blocknum]/index.vue
@@ -44,9 +44,10 @@ const query = `
 }
 `
 
+let block
 
 /* Make query request. */
-const block = await $fetch(ENDPOINT,
+const result = await $fetch(ENDPOINT,
     {
         method: 'POST',
         headers: {
@@ -57,20 +58,113 @@ const block = await $fetch(ENDPOINT,
     })
     .catch(err => console.error(err))
 
+if (result?.data?.block) {
+    block = result.data.block[0]
+}
+
 </script>
 
 <template>
-    <main class="">
+    <main v-if="block" class="">
         <main class="max-w-7xl mx-auto">
             <h1 class="text-4xl font-medium">
-                Block
+                Block # {{blocknum}}
             </h1>
 
-            <p>
-                {{blocknum}}
-            </p>
+            <BlockItem
+                title="Block Hash"
+                :value="block.hash"
+            />
+
+            <BlockItem
+                title="Confirmations"
+                :value="block.confirmations"
+            />
+
+            <BlockItem
+                title="Height"
+                :value="block.height"
+            />
+
+            <BlockItem
+                title="Size"
+                :value="block.size"
+            />
+
+            <BlockItem
+                title="Transaction Count"
+                :value="block.txcount"
+            />
+
+            <BlockItem
+                title="Fee Pool Amount"
+                :value="block.feePoolAmt"
+            />
+
+            <BlockItem
+                title="Merkle Root"
+                :value="block.merkleroot"
+            />
+
+            <BlockItem
+                title="Time"
+                :value="block.time"
+            />
+
+            <BlockItem
+                title="Median Time"
+                :value="block.mediantime"
+            />
+
+            <BlockItem
+                title="Nonce"
+                :value="block.nonce"
+            />
+
+            <BlockItem
+                title="Bits"
+                :value="block.bits"
+            />
+
+            <BlockItem
+                title="Difficulty"
+                :value="block.difficulty"
+            />
+
+            <BlockItem
+                title="Chainwork"
+                :value="block.chainwork"
+            />
+
+            <BlockItem
+                title="UTXO Commitment"
+                :value="block.utxoCommitment"
+            />
+
+            <BlockItem
+                title="Miner Data"
+                :value="block.minerData"
+            />
+
+            <BlockItem
+                title="Ancestor Hash"
+                :value="block.ancestorhash"
+            />
+
+            <BlockItem
+                title="Next Block Hash"
+                :value="block.nextblockhash"
+            />
 
             <pre>{{JSON.stringify(block, null, 2)}}</pre>
         </main>
+    </main>
+
+    <main v-else class="max-w-7xl mx-auto">
+        <div class="p-5 flex w-full justify-center">
+            <h1 class="text-4xl font-medium">
+                Oops! No block found
+            </h1>
+        </div>
     </main>
 </template>
