5c61161ad38788dda7cd04abd6551f0722b33d90
Ava's DAO Contributors <support@avasdao.org>
2023-03-28 04:47:58 -0400

    Refactor homepage.

--- web/components/BenefitsView.vue
+++ web/components/BenefitsView.vue
@@ -28,7 +28,7 @@
                         </p>
                         <div class="mt-6">
                             <a
-                                href="#"
+                                href="javascript://"
                                 class="inline-flex rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
                             >
                                 Get started
