dd6a152efa0ebf5b749c4db4bc2a133454f9525e
Ava's DAO Contributors <support@avasdao.org>
2023-04-02 23:03:53 -0400

    Enable Node clustering for GraphQL.

---
+++ graphql/cluster.js
@@ -0,0 +1,21 @@
+/* Import modules. */
+import cluster from 'cluster'
+import os from 'os'
+
+if (cluster.isPrimary) {
+    // Count the machine's CPUs
+    const cpuCount = os.cpus().length
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork()
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function () {
+        cluster.fork()
+    })
+} else {
+    // NOTE: Dynamic import of ES module.
+    import('/app/server.js')
+}
