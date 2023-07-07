386c144c5825bc1c85d9cc4bf3f7cd333ce47209
Ava's DAO Contributors <support@avasdao.org>
2023-07-07 08:51:29 -0400

    Add MetaNet page.

---
+++ web/pages/meta.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+useHead({
+    title: 'MetaNet — NexaShell',
+    meta: [
+        { name: 'description', content: 'MetaNet makes building your next BIG idea effortless.' }
+    ],
+})
+
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+
+/* Initialize System. */
+const System = useSystemStore()
+
+onBeforeMount(() => {
+    System.$state = JSON.parse(localStorage.getItem('system'))
+    // add additional states here...
+})
+
+watch(System.$state, (_state) => {
+    localStorage.setItem('system', JSON.stringify(_state))
+})
+// watch additional states here...
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+
+</script>
+
+<template>
+    <main class="max-w-7xl mx-auto py-5">
+        <h1 class="text-5xl font-medium">
+            MetaNet
+        </h1>
+
+        <p>
+            An EVM-compatible sidechain for Nexa.
+        </p>
+    </main>
+
+    <FooterView />
+</template>
