d26c6fec5d3faf58dd07cf9323f334ba29df34e0
Ava's DAO Contributors <support@avasdao.org>
2023-07-13 05:02:03 -0400

    Fix chg arrow.

--- web/components/FeaturedStats.vue
+++ web/components/FeaturedStats.vue
@@ -86,7 +86,7 @@ onMounted(() => {
                 <svg v-if="pctChg24h[0] === '-'" class="inline w-4 h-auto text-red-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"></path>
                 </svg>
-                <svg v-else class="inline w-4 h-auto text-green-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <svg v-if="pctChg24h[0] !== '-'" class="inline w-4 h-auto text-green-500" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"></path>
                 </svg>
             </h2>
