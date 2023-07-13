b54096668247d3b161dd06ffeb98d748d3a99450
Ava's DAO Contributors <support@avasdao.org>
2023-07-13 05:21:24 -0400

    Fix UI.

--- web/components/Search/Hero.vue
+++ web/components/Search/Hero.vue
@@ -6,7 +6,7 @@
             class="block w-full rounded-xl border-2 border-amber-400 py-5 pl-5 pr-14 text-xl text-gray-200 bg-gray-800 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-400"
         >
 
-        <div class="absolute inset-y-0 right-0 flex py-3 pr-3">
+        <div class="absolute inset-y-0 right-0 flex py-5 pr-3">
             <kbd class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">⌘K</kbd>
         </div>
     </main>
