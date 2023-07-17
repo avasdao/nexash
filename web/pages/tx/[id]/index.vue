433dd9f497de80f2bf06a04f145e37509611d2fb
Ava's DAO Contributors <support@avasdao.org>
2023-07-17 06:28:21 -0400

    Add cross-check link.

--- web/pages/tx/[id]/index.vue
+++ web/pages/tx/[id]/index.vue
@@ -134,6 +134,7 @@ onMounted(() => {
                         <option>Inputs</option>
                         <option>Outputs</option>
                         <option>Privacy Report</option>
+                        <option>Cross-check</option>
                     </select>
                 </div>
                 <div class="hidden sm:block">
@@ -163,6 +164,13 @@ onMounted(() => {
 
                             <NuxtLink :to="'/tx/' + id + '/privacy'" class="border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700 flex whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">
                                 Privacy Report
+                                <svg class="mx-1 w-3 h-auto" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
+                                </svg>
+                            </NuxtLink>
+
+                            <NuxtLink :to="'https://explorer.nexa.org/tx/' + id" target="_blank" class="border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700 flex whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">
+                                Cross-Check
                                 <svg class="mx-1 w-3 h-auto" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
                                 </svg>
