197c596fbc3cb83aca07ddd2a732bfcf9bc384ff
Ava's DAO Contributors <support@avasdao.org>
2023-09-26 23:48:33 -0400

    Fix object.

--- graphql/src/query/script.js
+++ graphql/src/query/script.js
@@ -26,11 +26,13 @@ export default {
     resolve: (_root, _args, _ctx) => {
         console.log('Script (args):', _args)
 
-        return [{
-            dust: '010203040506070809',
-            opcode: _args.opcode,
-            hex: _args.hex,
-        }]
+        return [
+            new GraphQLObjectType({
+                dust: '010203040506070809',
+                opcode: _args.opcode,
+                hex: _args.hex,
+            },
+        ]
     },
     description: `Request information about on-chain __Script__ transactions, eg. _OP_RETURN_ and _CashFusion._`,
 }
