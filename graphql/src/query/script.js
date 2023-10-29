944547c226b5e87e84b41e3d9a2e4008916bc189
Ava's DAO Contributors <support@avasdao.org>
2023-10-29 01:05:23 -0400

    dEBUG.

--- graphql/src/query/script.js
+++ graphql/src/query/script.js
@@ -103,6 +103,7 @@ export default {
 
         if (typeof _args?.hash === 'object') {
             hash = _args.hash
+            console.log('HASH', hash)
 
             if (Array.isArray(hash)) {
                 hash = hash[0] // FIXME Allow array of hashes.
