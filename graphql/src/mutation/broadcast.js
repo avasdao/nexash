31433a1ad18a451055d5db82eb583106e14deed9
Ava's DAO Contributors <support@avasdao.org>
2024-10-11 05:21:19 -0400

    Add debugging.

--- graphql/src/mutation/broadcast.js
+++ graphql/src/mutation/broadcast.js
@@ -1,5 +1,5 @@
 /* Import modules. */
-import { callNode } from '@nexajs/rpc'
+// import { callNode } from '@nexajs/rpc'
 
 import {
     GraphQLBoolean,
@@ -11,6 +11,94 @@ import {
     GraphQLSchema,
     GraphQLString,
 } from 'graphql'
+
+const callNode = async (_method, _params, _options) => {
+    /* Verify authentication. */
+    if (!username && !password && !_options) {
+        throw new Error('Oops! You have to connect to the local host first.')
+    }
+
+    if (_options) {
+        username = _options.username || null
+        password = _options.password || null
+        host = _options.host || '127.0.0.1'
+        port = _options.port || '7227'
+    }
+
+    if (!username || !password) {
+        throw new Error('Oops! You MUST provide a username and password to connect to the node.')
+    }
+
+    /* Initialize (local) variables. */
+    let endpoint
+    let error
+    let response
+
+    try {
+        /* Set endpoint. */
+        // endpoint = `http://${username}:${password}@${host}:${port}`
+        endpoint = `http://${host}:${port}`
+        // console.log('ENDPOINT', endpoint)
+
+        /* Build package. */
+        const pkg = {
+            "jsonrpc": "2.0",
+            "id": "core",
+            "method": _method,
+            "params": _params,
+        }
+        // console.log('PKG', pkg)
+
+        /* Set method. */
+        const method = 'POST'
+
+        /* Set headers. */
+        const headers = new Headers()
+        headers.append('Authorization', `Basic ${btoa(username + ':' + password)}`)
+        headers.append('Content-Type', 'application/json')
+
+        /* Make request. */
+        response = await fetch(endpoint, {
+            method,
+            headers,
+            body: JSON.stringify(pkg),
+        }).catch(_err => {
+            console.error('ERROR!', _err)
+
+            if (_err && _err.response && _err.response.text) {
+                error = JSON.parse(_err.response.text)
+            } else if (_err && _err.response) {
+                error = _err.response
+            } else {
+                error = _err
+            }
+        })
+
+        /* Validate error. */
+        if (error) {
+            return error
+        }
+
+        /* Decode response. */
+        response = await response.json()
+        console.log('RESONSE', response)
+
+        /* Validate response. */
+        if (!response) {
+            return null
+        }
+        // console.log('\nRPC CALL (response):', response)
+
+        /* Validate response. */
+        if (response && response.result) {
+            return response.result
+        } else {
+            return null
+        }
+    } catch (err) {
+        return err
+    }
+}
 
 export default {
     type: GraphQLString,
